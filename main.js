59e042be038fb257d221982bcbebaa19676959c5
Char <char@charcodes.online>
2024-10-09 12:05:32 +0100

    Deleted Main

--- main.js
+++
@@ -1,8 +0,0 @@
-const Load_Session = require("./files/load_session")
-
-Load_Session()
-
-const Upload_Plugin = require("./storage/upload")
-const Delete_Plugin = require("./storage/delete")
-
-Delete_Plugin()
