5189b8bfa1c56292be0c5ec0fb56b460f885d8af
Char <char@charcodes.online>
2024-12-01 10:17:20 -0800

    Added CLI name and description

--- main.ts
+++ main.ts
@@ -3,28 +3,37 @@ import { AuthCommand } from "./commands/auth.ts";
 import { BotCommand } from "./commands/bot.ts";
 import { PluginCommand } from "./commands/plugin.ts";
 
-
 const program = new Command();
 
-
 program
+  .name("blitz")
+  .version("1.0.0")
+  .description("A CLI tool for managing Blitz projects and plugins")
   .command("auth")
   .description("Authenticate using Discord OAuth")
   .action(() => {
     console.clear();
     AuthCommand();
-  })
+  });
+
+program
   .command("bot")
   .description("Create A New Blitz Bot")
   .action(() => {
     console.clear();
     BotCommand();
-  })
+  });
+
+program
   .command("plugin")
   .description("Create A New Blitz Plugin")
   .action(() => {
     console.clear();
     PluginCommand();
-  })
+  });
+
+
+
+
 
 program.parse(Deno.args);
