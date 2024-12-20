548563522dc82ee06f4992e742bbde59d3da2450
Char <68965006+C-h-a-r@users.noreply.github.com>
2024-12-20 18:27:16 +0000

    Update test.ts

--- commands/test.ts
+++ commands/test.ts
@@ -2,6 +2,6 @@ import { Bot } from "jsr:@blitz-bots/bot@1.5.0";
 
 export function TestCommand(token?: string, path?: string) {
   if (!token) throw new Error("No token provided.");
-  const bot = new Bot({ token: token, pluginsDir: "/" });
+  const bot = new Bot({ token: token, pluginsDir: "" });
   bot.start();
 }
