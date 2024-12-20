4c4c4e7c82f65e1c704873456918fe36c85e8cf9
Char <68965006+C-h-a-r@users.noreply.github.com>
2024-12-20 18:20:17 +0000

    Update test.ts

--- commands/test.ts
+++ commands/test.ts
@@ -1,4 +1,4 @@
-import { Bot } from "jsr:@blitz-bots/bot";
+import { Bot } from "jsr:@blitz-bots/bot@1.5.0";
 
 export function TestCommand(token?: string, path?: string) {
   if (!token) throw new Error("No token provided.");
